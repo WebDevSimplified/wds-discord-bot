51f185db9e029f1c321fb0cd539f686d39e52fac
Web Dev Simplified <kyle@webdevsimplified.com>
2023-04-01 10:38:12 -0500

    Make Work With React Simplified

--- courseCommands.js
+++ courseCommands.js
@@ -4,10 +4,16 @@ module.exports = [
     roleId: process.env.JAVASCRIPT_SIMPLIFIED_ROLE_ID,
     courseName: "JavaScript Simplified",
   },
+  // TODO: Remove learn-react-today command in a few weeks/months
   {
     command: `learn-react-today-${process.env.LEARN_REACT_TODAY_COMMAND_NUMBERS}`,
-    roleId: process.env.LEARN_REACT_TODAY_ROLE_ID,
-    courseName: "Learn React Today",
+    roleId: process.env.REACT_SIMPLIFIED_ROLE_ID,
+    courseName: "React Simplified",
+  },
+  {
+    command: `react-simplified-${process.env.REACT_SIMPLIFIED_COMMAND_NUMBERS}`,
+    roleId: process.env.REACT_SIMPLIFIED_ROLE_ID,
+    courseName: "React Simplified",
   },
   {
     command: `learn-css-today-${process.env.LEARN_CSS_TODAY_COMMAND_NUMBERS}`,

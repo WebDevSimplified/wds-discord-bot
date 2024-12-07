3a1109c73b6bd71b5deb89be53780eaf337bef77
Web Dev Simplified <kyle@webdevsimplified.com>
2024-12-07 09:55:40 -0600

    Add CSS Simplified Course

--- courseCommands.js
+++ courseCommands.js
@@ -4,12 +4,6 @@ module.exports = [
     roleId: process.env.JAVASCRIPT_SIMPLIFIED_ROLE_ID,
     courseName: "JavaScript Simplified",
   },
-  // TODO: Remove learn-react-today command in a few weeks/months
-  {
-    command: `learn-react-today-${process.env.LEARN_REACT_TODAY_COMMAND_NUMBERS}`,
-    roleId: process.env.REACT_SIMPLIFIED_ROLE_ID,
-    courseName: "React Simplified",
-  },
   {
     command: `react-simplified-${process.env.REACT_SIMPLIFIED_COMMAND_NUMBERS}`,
     roleId: process.env.REACT_SIMPLIFIED_ROLE_ID,
@@ -20,9 +14,15 @@ module.exports = [
     roleId: process.env.TYPESCRIPT_SIMPLIFIED_ROLE_ID,
     courseName: "TypeScript Simplified",
   },
+  // TODO: Remove learn-css-today command in a few weeks/months
   {
     command: `learn-css-today-${process.env.LEARN_CSS_TODAY_COMMAND_NUMBERS}`,
     roleId: process.env.LEARN_CSS_TODAY_ROLE_ID,
     courseName: "Learn CSS Today",
+  },
+  {
+    command: `css-simplified-${process.env.CSS_SIMPLIFIED_COMMAND_NUMBERS}`,
+    roleId: process.env.CSS_SIMPLIFIED_ROLE_ID,
+    courseName: "CSS Simplified",
   },
 ]
